9fb0c315d9aa6f5e2290d5b0dbee84bc290814a9
totto2727 <kaihatu.totto2727@gmail.com>
2023-04-27 15:50:47 +0900

    feat: MSWの設定を追加·

---
+++ app/server/page.tsx
@@ -0,0 +1,13 @@
+export default async function Home() {
+  const res = await fetch("https://my.backend/book");
+  const book = await res.json();
+
+  if (!book) return <div>loading</div>;
+  return (
+    <div>
+      <img src={book.imageUrl} alt={book.title} width="250" />
+      <h1>{book.title}</h1>
+      <p>{book.description}</p>
+    </div>
+  );
+}
